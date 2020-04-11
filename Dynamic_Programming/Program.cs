9e7882e81f8ab530e93099ca67266950e2b60191
Pouria Berahman <p.berahman@hotmail.com>
2020-04-11 09:01:29 +0300

    add part of dynamic programming chapter of hackerrank

---
+++ Dynamic_Programming/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dynamic_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem statment is 'Max Array Sum' -> https://bit.ly/2JSZQru
+            new HackerRank.Max_Array_Sum().maxSubsetSum(new int[5] { 3, 5, -7, 8, 10 });
+        }
+    }
+}
