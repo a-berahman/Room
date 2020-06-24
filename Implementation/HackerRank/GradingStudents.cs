ec3db2cb3626f4754282b2770bfabe6d648b5d48
Pouria Berahman <p.berahman@hotmail.com>
2020-06-24 10:03:45 +0300

    add Grading Students from hackerrank

---
+++ Implementation/HackerRank/GradingStudents.cs
@@ -0,0 +1,21 @@
+namespace Implementation.HackerRank
+{
+    public class GradingStudents
+    {
+        public List<int> gradingStudents(List<int> grades)
+        {
+
+            for (int i = 0; i < grades.Count; i++)
+            {
+                if (grades[i] < 38)
+                    continue;
+                var temp = grades[i];
+                while (temp % 5 != 0)
+                    temp++;
+
+                if (temp - grades[i] < 3) grades[i] = temp;
+            }
+            return grades;
+        }
+    }
+}
