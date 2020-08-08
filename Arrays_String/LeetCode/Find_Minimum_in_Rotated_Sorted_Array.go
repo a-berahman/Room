d94d300d2d05309813df2802458618f2855668d4
Pouria Berahman <a.berahman.dev@gmail.com>
2020-08-08 10:06:28 +0300

    add Find Minimum in Rotated Sorted Array from LeetCode

--- Arrays_String/LeetCode/Find_Minimum_in_Rotated_Sorted_Array.go
+++ Arrays_String/LeetCode/Find_Minimum_in_Rotated_Sorted_Array.go
@@ -1,9 +1,15 @@
-package leetCode
+package Find_Minimum_in_Rotated_Sorted_Array
 
 func findMin(nums []int) int {
 	left := 0
 	right := len(nums) - 1
 	for left < right {
-
+		mid := left + (right-left)/2
+		if nums[right] < nums[mid] {
+			left = mid + 1
+		} else {
+			right = mid
+		}
 	}
+	return nums[left]
 }
