254a5db17e5a40184425aca3d94d254e91237bf7
Ahmad <a.berahman.dev@gmail.com>
2020-09-12 10:16:10 +0430

    adding new challenge of leetcode

---
+++ LinkedLists/LeetCode/Odd_Even_Linked_List.go
@@ -0,0 +1,27 @@
+package LinkedList
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	odd := head
+	even := head.Next
+	evenHead := even
+
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		odd = odd.Next
+		even.Next = odd.Next
+		even = even.Next
+	}
+	odd.Next = evenHead
+
+	return head
+
+}
